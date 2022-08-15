67e9a4031f9053df3b3cbc6458ea28002bd4b71a
Precious Olusola <preciousolusola16@gmail.com>
2022-08-15 16:32:57 +0100

    advanced further, moving on to async login

--- src/features/posts/PostLists.jsx
+++ src/features/posts/PostLists.jsx
@@ -1,6 +1,7 @@
 import {useSelector} from 'react-redux';
 import PostAuthor from './PostAuthor';
 import { selectAllPosts } from './postsSlice';
+import ReactionButtons from './ReactionButtons';
 import TimeAgo from './TimeAgo';
 
 const PostsList = ()=>{
@@ -15,8 +16,8 @@ const PostsList = ()=>{
 
             <p className="postCredit">
                 <PostAuthor userId={post.userId}/>
-
                 <TimeAgo timestamp={post.date}/>
+                <ReactionButtons post={post}/>
             </p>
 
         </article>
