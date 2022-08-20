2dbb68043c8c5c0b8baa204d8e85420534705e86
Precious Olusola <preciousolusola16@gmail.com>
2022-08-20 10:09:05 +0100

    added user pages

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@
 import PostsList from './features/posts/PostLists';
 import AddPostForm from './features/posts/AddPostForm';
 import Layout from './components/Layout';
-import {Routes,Route} from 'react-router-dom';
+import {Routes,Route, Navigate} from 'react-router-dom';
 import SinglePostPage from './features/posts/SinglePostPage';
 import EditPostPage from './features/posts/EditPostPage';
+import UsersList from './features/users/UsersList';
+import UserPage from './features/users/UserPage';
+
 
 function App() {
   return (
@@ -18,6 +21,14 @@ function App() {
 
           <Route path="edit/:postId" element={<EditPostPage />}/>
         </Route>
+
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
+
+        {/* Catch all - replace with 404 component */}
+        <Route path="*" element={<Navigate to="/" replace/>}/>
 
       </Route>      
     </Routes>
--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -30,4 +30,4 @@ const usersSlice = createSlice({
 export const selectAllUsers = (state) => state.users;
 export const selectUserById = (state, userId) => state.users.find(user => user.id === userId);
 
-export default usersSlice.reducer
+export default usersSlice.reducer;
