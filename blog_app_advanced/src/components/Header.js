39151dd7d9266ed5b1553b65f7bb9c70ed87a24b
Precious Olusola <preciousolusola16@gmail.com>
2022-10-03 00:54:23 +0100

    added rtk builders and refactoring

--- blog_app_advanced/src/components/Header.js
+++ blog_app_advanced/src/components/Header.js
@@ -1,11 +1,9 @@
 import { Link } from "react-router-dom"
-import { useDispatch, useSelector } from "react-redux"
-import { increaseCount, getCount } from "../features/posts/postsSlice"
+
+
 
 const Header = () => {
-    const dispatch = useDispatch()
-    const count = useSelector(getCount)
-
+    
     return (
         <header className="Header">
             <h1>Redux Blog</h1>
@@ -15,11 +13,6 @@ const Header = () => {
                     <li><Link to="post">Post</Link></li>
                     <li><Link to="user">Users</Link></li>
                 </ul>
-                <button
-                    onClick={() =>
-                        dispatch(increaseCount())
-                    }
-                >{count}</button>
             </nav>
         </header>
     )
--- blog_app_advanced/src/features/posts/ReactionButtons.js
+++ blog_app_advanced/src/features/posts/ReactionButtons.js
@@ -1,5 +1,4 @@
-import { useDispatch } from "react-redux";
-import { reactionAdded } from "./postsSlice";
+import { useAddReactionMutation } from "./postsSlice";
 
 const reactionEmoji = {
     thumbsUp: '👍',
@@ -10,17 +9,19 @@ const reactionEmoji = {
 }
 
 const ReactionButtons = ({ post }) => {
-    const dispatch = useDispatch()
+    const [addReaction] = useAddReactionMutation();
 
-    const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
+    const reactionButtons = Object.entries(reactionEmoji).map(( [name, emoji] ) => {
         return (
             <button
                 key={name}
                 type="button"
                 className="reactionButton"
-                onClick={() =>
-                    dispatch(reactionAdded({ postId: post.id, reaction: name }))
-                }
+                onClick={ () =>{
+                    const newValue = post.reactions[name] +1;
+
+                    addReaction({ postId: post.id, reaction: {...post.reactions, [name]:newValue } });
+                }}
             >
                 {emoji} {post.reactions[name]}
             </button>
@@ -29,4 +30,6 @@ const ReactionButtons = ({ post }) => {
 
     return <div>{reactionButtons}</div>
 }
-export default ReactionButtons
+
+
+export default ReactionButtons;
